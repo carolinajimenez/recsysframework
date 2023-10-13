b044f3e06601d2c307666c8cce2d7cf8b9106f80
Carolina Jimenez Moreno <CJimenezm0794@gmail.com>
2023-10-13 10:34:21 -0500

    Update framework folder name + Update instructions in create_project.sh file

---
+++ README.md
@@ -0,0 +1,28 @@
+Usage
+------------
+
+1. Clone the repository.
+    ```
+    git clone https://github.com/carolinajimenez/recsysframework.git
+    cd recsysframework
+    ```
+
+2. Give execution permissions to the creation script.    
+Before you can run the script, you must give it execution permissions. You can do this with the following command:
+    ```
+    chmod +x create_project.sh
+    ```
+
+3. Create projects.    
+With this command the '*python-project*' will be created using `cookiecutter` from the custom template '**RecSysFramework**'. The 'helm-project' will also be created with Helm which will allow you to deploy to the Kubernetes cluster later on.
+    ```
+    ./create_project.sh
+    ```
+
+4. Proceed to organize the development of the '*python-project*'.
+
+5. Follow the instructions in the '**README.md**' of the '*python-project*' created with `cookiecutter`. Here you will find instructions on how to build and run the Docker image, once done, push the image to a repository, either Docker Hub or other.
+
+
+
+<small>[Complete Helm Chart Tutorial: From Beginner to Expert Guide](https://www.youtube.com/watch?v=DQk8HOVlumI)</small>
--- create_project.sh
+++ create_project.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-# Solicitar la ruta del entorno virtual
-echo "Por favor, ingresa la ruta completa del entorno virtual:"
+# Detect the operating system
+OS="$(uname)"
+
+# Install jq and other necessary packages
+install_packages() {
+    if [ "$OS" == "Darwin" ]; then  # macOS
+        if ! command -v jq &> /dev/null; then
+            echo "Installing jq..."
+            brew install jq
+        fi
+    elif [ "$OS" == "Linux" ]; then  # Linux
+        if ! command -v jq &> /dev/null; then
+            echo "Installing jq..."
+            sudo apt-get update
+            sudo apt-get install -y jq
+        fi
+    else
+        echo "Unsupported operating system."
+        exit 1
+    fi
+}
+
+# Call the function to install packages
+install_packages
+
+# Request the full path of the virtual environment
+echo "Please enter the full path of the virtual environment:"
 read venv_path
 
-# Activar el entorno virtual
+# Activate the virtual environment
 source $venv_path/bin/activate
 
-# Verificar que el entorno virtual se activó correctamente
+# Verify that the virtual environment was activated correctly
 if [ $? -ne 0 ]; then
-    echo "Error al activar el entorno virtual. Asegúrate de proporcionar la ruta correcta."
+    echo "Error activating the virtual environment. Ensure you provide the correct path."
     exit 1
 fi
 
-# Paso 1: Ejecutar cookiecutter
+# Step 1: Execute cookiecutter
 cookiecutter RecSysFramework
 
-# Desactivar el entorno virtual
+# Deactivate the virtual environment
 deactivate
 
-# Paso 2: Ejecutar helm
+# Detect the directory of the project created by cookiecutter
+project_dir=$(ls -d */ | grep -v "RecSysFramework" | head -n 1)
+
+# Read the Docker repository name from the answers.json file
+docker_repo_name=$(jq -r '.docker_image_name' "$project_dir/answers.json")
+
+# Step 2: Execute helm
 helm create recsyshelm
+
+# Update the values.yaml file with the Docker repository name
+if [ "$OS" == "Darwin" ]; then  # macOS
+    sed -i '' "s|repository: nginx|repository: $docker_repo_name|" recsyshelm/values.yaml
+elif [ "$OS" == "Linux" ]; then  # Linux
+    sed -i "s|repository: nginx|repository: $docker_repo_name|" recsyshelm/values.yaml
+fi
